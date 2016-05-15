a5eb2a89ed22b3c4e1af02a22be52753b5ca8a8c
JiHunJeon <gaver1213@naver.com>
2016-05-15 15:37:47 +0900

    compose layout

--- app/controllers/cover_letters_controller.rb
+++ app/controllers/cover_letters_controller.rb
@@ -1,12 +1,13 @@
 class CoverLettersController < ApplicationController
   before_action :set_cover_letter, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [ :index, :new, :edit, :create, :update, :destroy ]
+  before_action :set_user
 #  before_action :authenticate_profile, only: [ :index ]
 
   # GET /cover_letters
   # GET /cover_letters.json
   def index
-    @cover_letters = CoverLetter.all
+    @cover_letters = current_user.cover_letters.all.order('id desc').paginate(:page => params[:page], :per_page => 5)
   end
 
   # GET /cover_letters/1
@@ -17,23 +18,19 @@ class CoverLettersController < ApplicationController
   # GET /cover_letters/new
   def new
     @cover_letter = CoverLetter.new
-    @user = current_user
     @cover_letter.companies.build
   end
 
   # GET /cover_letters/1/edit
   def edit
-    @cover_letter = CoverLetter.find(params[:id])
-
+    @cover_letter = current_user.cover_letters.find(params[:id])
     @cover_letter.companies.build if @cover_letter.companies.last().nil?
-
-    @user = current_user
   end
 
   # POST /cover_letters
   # POST /cover_letters.json
   def create
-    @cover_letter = CoverLetter.new(cover_letter_params)
+    @cover_letter = current_user.cover_letters.new(cover_letter_params)
 
     respond_to do |format|
       if @cover_letter.save
@@ -49,6 +46,7 @@ class CoverLettersController < ApplicationController
   # PATCH/PUT /cover_letters/1
   # PATCH/PUT /cover_letters/1.json
   def update
+    @cover_letter = current_user.cover_letters.find(params[:id])
     respond_to do |format|
       if @cover_letter.update(cover_letter_params)
         format.html { redirect_to @cover_letter, notice: 'Cover letter was successfully updated.' }
@@ -63,7 +61,8 @@ class CoverLettersController < ApplicationController
   # DELETE /cover_letters/1
   # DELETE /cover_letters/1.json
   def destroy
-    @cover_letter.destroy
+    @cover_letter = current_user.cover_letters.find(params[:id]).destroy
+    #@cover_letter.destroy
     respond_to do |format|
       format.html { redirect_to cover_letters_url, notice: 'Cover letter was successfully destroyed.' }
       format.json { head :no_content }
@@ -75,6 +74,10 @@ class CoverLettersController < ApplicationController
     def set_cover_letter
       @cover_letter = CoverLetter.find(params[:id])
     end
+
+  def set_user
+    @user = current_user
+  end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def cover_letter_params
